19d5038ab611c083f292eef9f1c3c906361d4c3e
David Whitlatch <davidtwhitlatch@gmail.com>
2020-09-11 16:41:05 -0400

    startedauth from FGE

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Tasteville</h1>
+      <Link to='/login'>Login/Register</Link>
+      <hr />
+    </header>
+  )
+}
---
+++ client/src/screens/Login.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Login() {
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  })
+  const { username, password } = formData;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+
+  }
+
+  return (
+    <form>
+      <h3>Login</h3>
+      <label>Username:
+        <input
+          type='text'
+          name='username'
+          value={username}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <label>Password:
+        <input
+          type='password'
+          name='password'
+          value={password}
+          onChange={handleChange}
+        />
+      </label>
+      <br />
+      <Link to='/register'>Register</Link>
+      <button>Submit</button>
+    </form>
+  )
+}
